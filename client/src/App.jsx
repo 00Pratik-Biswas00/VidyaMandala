e06d5b35892bc1dab5ae5e9effec74fa6b3aced9
Pratik Biswas <pratik_biswas@epam.com>
2025-04-24 18:08:51 +0530

    initial setup is done

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <section>
+      <h1 className="text-3xl font-bold ">
+      Let's make Vidyamandala!
+    </h1>
+    </section>
+  )
+}
+
+export default App
